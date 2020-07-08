011adf254a7e4dc10f3ffc4cf9ad3e2daacbd13b
Shanzid Shaiham <shanzid.shaiham@gmail.com>
2020-07-08 01:10:41 -0400

    Lint fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,7 @@ module.exports = {
   },
   rules: {
     "prettier/prettier": "error",
+    "@typescript-eslint/no-explicit-any": 0,
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
   },
